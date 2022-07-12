bf3cebaa99bfa40d3dbce04e6b6e73c8527afe73
Hasnain Qari <qarihasnain43@gmail.com>
2022-07-12 11:24:19 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,157 @@
+// Chapter No 35 to 38;
+function log(a) {
+    console.log(a);
+}
+
+// Question No 01;
+// function date (){
+//     var now = new Date();
+//     log(now);
+// }
+// date();
+
+// Question No 02;
+// function userName (){
+//     var firstName = prompt("Enter First Name");
+//     var lastName = prompt("Enter Last Name");
+//     log(firstName + "" + lastName);
+// }
+// userName();
+
+// Question No 03;
+// function addition (first,second){
+//     log(first+second);
+// }
+// addition(500,400);
+
+// Question No 04;
+// function calculation(){
+// var num1 = +prompt("Enter a First Number");
+// var num2 = +prompt("Enter a Second Number");
+// var opt = prompt("Enter a Operator\nWhat do you want to calculate (+ - * / %)");
+// var answer;
+// if (opt === "+") {
+//     answer = num1 + num2
+// } else if (opt === "-") {
+//     answer = num1 - num2
+// } else if (opt === "*") {
+//     answer = num1 * num2
+// } else if (opt === "/") {
+//     answer = num1 / num2
+// } else if (opt === "%") {
+//     answer = num1 % num2
+// }
+//     log(answer);
+// }
+// calculation();
+
+// Question No 05;
+// function squares (){
+//     var multiply = +prompt("Enter a number");
+//     var result = (multiply);
+//     log(result);
+// }
+// squares();
+
+// Question No 06;
+// function factorial (a,b,c){
+//     var result = (a*b*c);
+//     log(result);
+// }
+// factorial(5,3,2);
+
+// Question No 07;
+// function numbers (){
+//     var firstNum = (1);
+//     log(result);
+// }
+// numbers();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// class code;
+// function foo (a,b){
+//     if(a + b){
+//     log(a,b);
+// }
+// }
+// foo ("Hello","World");
+// function printTable(t){
+//     var table = +prompt("Enter No Of Table");
+//     for(var i = 1; i <= 10; i++){
+//     log(table + " x " + i + " = " + table*i );
+// };
+// }
+// printTable();
+// class code ;
